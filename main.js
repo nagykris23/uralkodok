4de54fc2040203f8643fb2ecee84c4fd165d0221
Kristof <nagykris68@gmail.com>
2025-02-19 20:19:27 +0100

    változók bevezetése

--- main.js
+++ main.js
@@ -7,6 +7,34 @@ table.appendChild(tbody)// a tbody elemet hozzáadom a table-hoz
 const thead = document.createElement('thead')// létrehozok egy thead elemet
 table.appendChild(thead)// a thead elemet hozzáadom a table-hoz
 
+//változok létrehozása
+const fejléc1 = "Uralkodó"// fejléc első változó létrehozása
+const fejléc2 = "Esemény"//fejléc masodik változó létrehozása 
+const fejléc3 = "Évszám"//fejléc harmadik változó létrehozása
+
+const elsosor1 = "I. István"// első sor első változó létrehozása
+const masodiksor1 = "Koronázás"// első sor masodik változó létrehozása
+const harmadiksor1 = "1000"// első sor harmadik változó létrehozása
+const elsooszlop1 = "Pannonhalmi apátság megalapítása"// első sor második esemény változó létrehozása
+const masodikoszlop1 = "1001"// első sor második évszám változó létrehozása
+
+const elsosor2 = "IV. Béla"// második sor első változó létrehozása
+const masodiksor2 = "tatárjárás"// második sor masodik változó létrehozása
+const harmadiksor2 = "1241-1242"// második sor harmadik változó létrehozása
+
+const elsosor3 = "Mátyás király"// harmadik sor első változó létrehozása
+const masodiksor3 = "Bécs elfoglalása"// harmadik sor masodik változó létrehozása
+const harmadiksor3 = "1485"// harmadik sor harmadik változó létrehozása
+const elsooszlop2 = "Kenyérmzei csata"// harmadik sor második esemény változó létrehozása
+const masodikoszlop2 = "1479"// harmadik sor második évszám változó létrehozása
+
+const elsosor4 = "II. Rákóczi Ferenc"// negyedik sor első változó létrehozása
+const masodiksor4 = "A szabadságharc kezdete"// negyedik sor masodik változó létrehozása
+const harmadiksor4 = "1703"// negyedik sor harmadik változó létrehozása
+const elsooszlop3 = "szabadságharc vége"// negyedik sor második esemény változó létrehozása
+const masodikoszlop3 = "1711"// negyedik sor második évszám változó létrehozása
+
+
 //colgroup létrehozása
 const colgroup = document.createElement('colgroup')// létrehozok egy colgroup elemet
 table.appendChild(colgroup)// a colgroup elemet hozzáadom a table-hoz
@@ -28,15 +56,15 @@ thead.appendChild(tr)// a tr elemet hozzáadom a thead-hoz
 
 const th1 = document.createElement('th')// létrehozok egy th elemet
 tr.appendChild(th1)// a th elemet hozzáadom a tr-hoz
-th1.textContent = 'Uralkodó'// a th1-nek beállítom a szövegét
+th1.innerHTML = fejléc1 // a th1-nek beállítom a szövegét
 
 const th2 = document.createElement('th')// létrehozok egy th elemet
 tr.appendChild(th2)// a th elemet hozzáadom a tr-hoz
-th2.textContent = 'Esemény'// a th2-nek beállítom a szövegét
+th2.innerHTML = fejléc2// a th2-nek beállítom a szövegét
 
 const th3 = document.createElement('th')// létrehozok egy th elemet
 tr.appendChild(th3)// a th elemet hozzáadom a tr-hoz
-th3.textContent = 'Évszám'// a th3-nek beállítom a szövegét
+th3.innerHTML = fejléc3// a th3-nek beállítom a szövegét
 
 //elso sor hozzáadása a táblázathoz
 
@@ -46,15 +74,15 @@ tbody.appendChild(tr1)// a tr elemet hozzáadom a tbody-hoz
 const elsocella = document.createElement('td')// létrehozok egy td elemet
 tr1.appendChild(elsocella)// a td elemet hozzáadom a tr1-hez
 elsocella.rowSpan = 2// az elsocella td-nek beállítom a rowspan-jét
-elsocella.textContent = 'I. István'// az elsocella td-nek beállítom a szövegét
+elsocella.innerHTML=elsosor1// az elsocella td-nek beállítom a szövegét
 
 const masodikcella = document.createElement('td')// létrehozok egy td elemet
 tr1.appendChild(masodikcella)// a td elemet hozzáadom a tr1-hez
-masodikcella.textContent = 'Koronázás'// a masodikcella td-nek beállítom a szövegét
+masodikcella.innerHTML = masodiksor1// a masodikcella td-nek beállítom a szövegét
 
 const harmadikcella = document.createElement('td')// létrehozok egy td elemet
 tr1.appendChild(harmadikcella)// a td elemet hozzáadom a tr1-hez
-harmadikcella.textContent = '1000'// a harmadikcella td-nek beállítom a szövegét
+harmadikcella.innerHTML = harmadiksor1// a harmadikcella td-nek beállítom a szövegét
 
 //masodik sor hozzáadása a táblázathoz
 
@@ -63,11 +91,11 @@ tbody.appendChild(tr2)// a tr elemet hozzáadom a tbody-hoz
 
 const elsocellasor2 = document.createElement('td')// létrehozok egy td elemet
 tr2.appendChild(elsocellasor2)// a td elemet hozzáadom a tr2-hoz
-elsocellasor2.textContent = 'Pannonhalmi apátság megalapítása '// az elsocellasor2 td-nek beállítom a szövegét
+elsocellasor2.innerHTML = elsooszlop1// az elsocellasor2 td-nek beállítom a szövegét
 
 const masodikcellasor2 = document.createElement('td')// létrehozok egy td elemet
 tr2.appendChild(masodikcellasor2)// a td elemet hozzáadom a tr2-hoz
-masodikcellasor2.textContent = '1001'// a masodikcellasor2 td-nek beállítom a szövegét
+masodikcellasor2.innerHTML=masodikoszlop1// a masodikcellasor2 td-nek beállítom a szövegét
 
 //harmadik sor hozzáadása a táblázathoz
 
@@ -76,15 +104,15 @@ tbody.appendChild(tr3)// a tr elemet hozzáadom a tbody-hoz
 
 const elsocellasor3 = document.createElement('td')// létrehozok egy td elemet
 tr3.appendChild(elsocellasor3)// a td elemet hozzáadom a tr3-hoz
-elsocellasor3.textContent = 'IV. Béla'// az elsocellasor3 td-nek beállítom a szövegét
+elsocellasor3.innerHTML = elsosor2// az elsocellasor3 td-nek beállítom a szövegét
 
 const masodikcellasor3 = document.createElement('td')// létrehozok egy td elemet
 tr3.appendChild(masodikcellasor3)// a td elemet hozzáadom a tr3-hoz
-masodikcellasor3.textContent = 'tatárjárás'// a masodikcellasor3 td-nek beállítom a szövegét
+masodikcellasor3.innerHTML = masodiksor2 // a masodikcellasor3 td-nek beállítom a szövegét
 
 const harmadikcellasor3 = document.createElement('td')// létrehozok egy td elemet
 tr3.appendChild(harmadikcellasor3)// a td elemet hozzáadom a tr3-hoz
-harmadikcellasor3.textContent = '1241-1242'// a harmadikcellasor3 td-nek beállítom a szövegét
+harmadikcellasor3.innerHTML = harmadiksor2// a harmadikcellasor3 td-nek beállítom a szövegét
 
 //negyedik sor hozzáadása a táblázathoz
 
@@ -94,15 +122,15 @@ tbody.appendChild(tr4)// a tr elemet hozzáadom a tbody-hoz
 const elsocellasor4 = document.createElement('td')// létrehozok egy td elemet
 tr4.appendChild(elsocellasor4)// a td elemet hozzáadom a tr4-hoz
 elsocellasor4.rowSpan = 2// az elsocellasor4 td-nek beállítom a rowspan-jét
-elsocellasor4.textContent = 'Mátyás király'// az elsocellasor4 td-nek beállítom a szövegét
+elsocellasor4.innerHTML=  elsosor3// az elsocellasor4 td-nek beállítom a szövegét
 
 const masodikcellasor4 = document.createElement('td')// létrehozok egy td elemet
 tr4.appendChild(masodikcellasor4)// a td elemet hozzáadom a tr4-hoz
-masodikcellasor4.textContent = 'Bécs elfoglalása'// a masodikcellasor4 td-nek beállítom a szövegét
+masodikcellasor4.innerHTML = masodiksor3// a masodikcellasor4 td-nek beállítom a szövegét
 
 const harmadikcellasor4 = document.createElement('td')// létrehozok egy td elemet
 tr4.appendChild(harmadikcellasor4)// a td elemet hozzáadom a tr4-hoz
-harmadikcellasor4.textContent = '1485'// a harmadikcellasor4 td-nek beállítom a szövegét
+harmadikcellasor4.innerHTML= harmadiksor3// a harmadikcellasor4 td-nek beállítom a szövegét
 
 //ötödik sor hozzáadása a táblázathoz
 
@@ -111,11 +139,11 @@ tbody.appendChild(tr5)// a tr elemet hozzáadom a tbody-hoz
 
 const elsocellasor5 = document.createElement('td')// létrehozok egy td elemet
 tr5.appendChild(elsocellasor5)// a td elemet hozzáadom a tr5-hoz
-elsocellasor5.textContent = 'Kenyérmzei csata'// az elsocellasor5 td-nek beállítom a szövegét
+elsocellasor5.innerHTML = elsooszlop2// az elsocellasor5 td-nek beállítom a szövegét
 
 const masodikcellasor5 = document.createElement('td')// létrehozok egy td elemet
 tr5.appendChild(masodikcellasor5)// a td elemet hozzáadom a tr5-hoz
-masodikcellasor5.textContent = '1479'// a masodikcellasor5 td-nek beállítom a szövegét
+masodikcellasor5.innerHTML = masodikoszlop2// a masodikcellasor5 td-nek beállítom a szövegét
 
 //hatodik sor hozzáadása a táblázathoz
 
@@ -125,15 +153,15 @@ tbody.appendChild(tr6)// a tr elemet hozzáadom a tbody-hoz
 const elsocellasor6 = document.createElement('td')// létrehozok egy td elemet
 tr6.appendChild(elsocellasor6)// a td elemet hozzáadom a tr6-hoz
 elsocellasor6.rowSpan = 2// az elsocellasor6 td-nek beállítom a rowspan-jét
-elsocellasor6.textContent = 'II. Rákóczi Ferenc'// az elsocellasor6 td-nek beállítom a szövegét
+elsocellasor6.innerHTML =elsosor4// az elsocellasor6 td-nek beállítom a szövegét
 
 const masodikcellasor6 = document.createElement('td')// létrehozok egy td elemet
 tr6.appendChild(masodikcellasor6)// a td elemet hozzáadom a tr6-hoz
-masodikcellasor6.textContent = 'Rákóczi szabadságharc'// a masodikcellasor6 td-nek beállítom a szövegét
+masodikcellasor6.innerHTML = masodiksor4// a masodikcellasor6 td-nek beállítom a szövegét
 
 const harmadikcellasor6 = document.createElement('td')// létrehozok egy td elemet
 tr6.appendChild(harmadikcellasor6)// a td elemet hozzáadom a tr6-hoz
-harmadikcellasor6.textContent = '1703'// a harmadikcellasor6 td-nek beállítom a szövegét
+harmadikcellasor6.innerHTML = harmadiksor4// a harmadikcellasor6 td-nek beállítom a szövegét
 
 //hetedik sor hozzáadása a táblázathoz
 
@@ -142,9 +170,9 @@ tbody.appendChild(tr7)// a tr elemet hozzáadom a tbody-hoz
 
 const elsocellasor7 = document.createElement('td')// létrehozok egy td elemet
 tr7.appendChild(elsocellasor7)// a td elemet hozzáadom a tr7-hoz
-elsocellasor7.textContent = 'szabadságharc vége'// a elsoellasor7 td-nek beállítom a szövegét
+elsocellasor7.innerHTML = elsooszlop3// a elsoellasor7 td-nek beállítom a szövegét
 
 const masodikcellasor7 = document.createElement('td')// létrehozok egy td elemet
 tr7.appendChild(masodikcellasor7)// a td elemet hozzáadom a tr7-hoz
-masodikcellasor7.textContent = '1711'// a masodikcellasor7 td-nek beállítom a szövegét
+masodikcellasor7.innerHTML=masodikoszlop3// a masodikcellasor7 td-nek beállítom a szövegét
 
