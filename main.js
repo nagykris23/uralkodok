b1862ca00591a5fd5f6d4897a6adfe7194bf295b
Kristof <nagykris68@gmail.com>
2025-02-20 20:19:34 +0100

    osszetett validacio fuggvenybe szervezese

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 const array = [
     {
         uralkodo: "I. István",//uralkodó változó létrehozása
-        esemeny: "Koronázás",//esemény változó létrehozása
-        evszam: "1000",//évszám változó létrehozása
+        esemeny1: "Koronázás",//esemény1 változó létrehozása
+        evszam1: "1000",//évszám1 változó létrehozása
         esemeny2: "Pannonhalmi apátság megalapítása",//esemény2 változó létrehozása
         evszam2: "1001"//évszám2 változó létrehozása
     },
@@ -14,15 +14,15 @@ const array = [
     },
     {
         uralkodo: "Mátyás király",//uralkodó változó létrehozása
-        esemeny: "Bécs elfoglalása",//esemény változó létrehozása
-        evszam: "1485",//évszám változó létrehozása
+        esemeny1: "Bécs elfoglalása",//esemény1 változó létrehozása
+        evszam1: "1485",//évszám1 változó létrehozása
         esemeny2: "Kenyérmzei csata",//esemény2 változó létrehozása
         evszam2: "1479"//évszám2 változó létrehozása
     },
     {
         uralkodo: "II. Rákóczi Ferenc",//uralkodó változó létrehozása
-        esemeny: "A szabadságharc kezdete",//esemény változó létrehozása
-        evszam: "1703",//évszám változó létrehozása
+        esemeny1: "A szabadságharc kezdete",//esemény1 változó létrehozása
+        evszam1: "1703",//évszám1 változó létrehozása
         esemeny2: "szabadságharc vége",//esemény2 változó létrehozása
         evszam2: "1711"//évszám2 változó létrehozása
     }
@@ -87,11 +87,11 @@ function rendermenu() {
 
         const td2 = document.createElement('td')//létrehozok egy td elemet
         tr.appendChild(td2)//a td elemet hozzáadom a tr-hoz
-        td2.innerHTML = adat.esemeny//a td2-nek beállítom a szövegét
+        td2.innerHTML = adat.esemeny1 || adat.esemeny//a td2-nek beállítom a szövegét
 
         const td3 = document.createElement('td')//létrehozok egy td elemet
         tr.appendChild(td3)//a td elemet hozzáadom a tr-hoz
-        td3.innerHTML = adat.evszam//a td3-nek beállítom a szövegét
+        td3.innerHTML = adat.evszam1 || adat.evszam//a td3-nek beállítom a szövegét
 
         //elágazás létrehozása
         if (adat.esemeny2 && adat.evszam2) {
@@ -115,8 +115,8 @@ rendermenu()//függvény meghívása
 document.getElementById('form').addEventListener('submit', function (e) {
     e.preventDefault()//megakadályozza az alapértelmezett eseményt
     const uralkodo = document.getElementById('uralkodo_nev')//lekéri az uralkodo mező értékét
-    const esemeny = document.getElementById('esemeny1')//lekéri az esemeny mező értékét
-    const evszam = document.getElementById('evszam1')//lekéri az evszam mező értékét
+    const esemeny1 = document.getElementById('esemeny1')//lekéri az esemeny1 mező értékét
+    const evszam1 = document.getElementById('evszam1')//lekéri az evszam1 mező értékét
     const esemeny2 = document.getElementById('esemeny2')//lekéri az esemeny2 mező értékét
     const evszam2 = document.getElementById('evszam2')//lekéri az evszam2 mező értékét
 
@@ -124,17 +124,17 @@ document.getElementById('form').addEventListener('submit', function (e) {
     if (!validatform(uralkodo, "kötelező megadni az uralkodót", 'uralkodo_error')) {//ha a validatform függvény értéke hamis, akkor
         isvalid = false//isvalid változó értékét hamisra állítja
     }
-    if (!validatform(esemeny, "kötelező megadni az eseményt", 'esemeny1_error')) {//ha a validatform függvény értéke hamis, akkor
+    if (!validatform(esemeny1, "kötelező megadni az eseményt", 'esemeny1_error')) {//ha a validatform függvény értéke hamis, akkor
         isvalid = false//isvalid változó értékét hamisra állítja
     }
-    if (!validatform(evszam, "kötelező megadni az évszámot", 'evszam1_error')) {//ha a validatform függvény értéke hamis, akkor
+    if (!validatform(evszam1, "kötelező megadni az évszámot", 'evszam1_error')) {//ha a validatform függvény értéke hamis, akkor
         isvalid = false//isvalid változó értékét hamisra állítja
     }
-    if ((esemeny2 && !evszam2) || (!esemeny2 && evszam2)) {//ha az esemeny2 mezőben van érték, de az evszam2 mezőben nincs, vagy az esemeny2 mezőben nincs érték, de az evszam2 mezőben van, akkor
-        if (!esemeny2) {//ha az esemeny2 mezőben nincs érték, akkor
+    if ((esemeny2.value && !evszam2.value) || (!esemeny2.value && evszam2.value)) {//ha az esemeny2 mezőben van érték, de az evszam2 mezőben nincs, vagy az esemeny2 mezőben nincs érték, de az evszam2 mezőben van, akkor
+        if (!esemeny2.value) {//ha az esemeny2 mezőben nincs érték, akkor
             document.getElementById('esemeny2_error').innerHTML = 'Kötelező megadni az eseményt'//kiírja a hibaüzenetet
         }
-        if (!evszam2) {//ha az evszam2 mezőben nincs érték, akkor
+        if (!evszam2.value) {//ha az evszam2 mezőben nincs érték, akkor
             document.getElementById('evszam2_error').innerHTML = 'Kötelező megadni az évszámot'//kiírja a hibaüzenetet
         }
         isvalid = false//isvalid változó értékét hamisra állítja
@@ -143,8 +143,8 @@ document.getElementById('form').addEventListener('submit', function (e) {
     if (isvalid) {//ha az isvalid változó értéke igaz, akkor
         const ujuralkodo = {
             uralkodo: uralkodo.value,//uralkodo változó létrehozása
-            esemeny: esemeny.value,//esemeny változó létrehozása
-            evszam: evszam.value,//evszam változó létrehozása
+            esemeny1: esemeny1.value,//esemeny1 változó létrehozása
+            evszam1: evszam1.value,//evszam1 változó létrehozása
             esemeny2: esemeny2.value,//esemeny2 változó létrehozása
             evszam2: evszam2.value//evszam2 változó létrehozása
         }
@@ -153,7 +153,7 @@ document.getElementById('form').addEventListener('submit', function (e) {
         rendermenu()//függvény meghívása
     }
 })
-//összetett vaLidáló függvény
+//összetett validálás függvénye
 function validatform(inputHtmlElement, errorMessage, errorElementId) {//validalo fuggveny
     let valid = true//kezdetben igaz
     if (inputHtmlElement.value === '') {//ha ures
